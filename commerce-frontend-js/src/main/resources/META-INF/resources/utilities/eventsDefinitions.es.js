a43673f9f0104fd8177b681e6a4a384a9f6911d1
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-11-04 15:17:01 +0100

    COMMERCE-2225 dataset displays can now be updated from the outside

--- commerce-frontend-js/src/main/resources/META-INF/resources/utilities/eventsDefinitions.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/utilities/eventsDefinitions.es.js
@@ -1,9 +1,11 @@
+export const CURRENT_PRODUCT_UPDATED = 'current-product-updated';
+export const CURRENT_PRODUCT_STATUS_CHANGED = 'current-product-status-changed'
+export const IFRAME_LOADED = 'iframe-loaded';
 export const OPEN_SIDE_PANEL = 'open-side-panel';
 export const OPEN_MODAL = 'open-modal';
 export const OPEN = 'open-component';
-export const CURRENT_PRODUCT_UPDATED = 'current-product-updated';
 export const PRODUCT_OPTIONS_CHANGED = 'product-options-changed';
-export const CURRENT_PRODUCT_STATUS_CHANGED = 'current-product-status-changed'
+export const UPDATE_FROM_COMPONENT = 'update-from-component';
 
 export default {
 	OPEN,
